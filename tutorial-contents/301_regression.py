30bf8f9de6056aa7d7e88acc5f0263e1a3118284
WanYi <986371154@qq.com>
2019-10-30 21:45:00 +0800

    Update 301_regression.py

--- tutorial-contents/301_regression.py
+++ tutorial-contents/301_regression.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # torch.manual_seed(1)    # reproducible
 
-x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1) 把一维的数据变成二维，torch中只会处理二维数据
 y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
 
 # torch can only train on Variable, so convert them to Variable
@@ -22,7 +22,7 @@ y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor),
 # plt.scatter(x.data.numpy(), y.data.numpy())
 # plt.show()
 
-
+# 构建神经网络
 class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden, n_output):
         super(Net, self).__init__()
